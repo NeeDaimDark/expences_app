f7a411adde09fb5b26c353baccab9855244360f4
unknown <mohamedamine.koubaa@esprit.tn>
2025-10-21 17:32:48 +0100

    fixing addimg and deleting expense

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -40,13 +40,38 @@ class _ExpensesState extends State<Expenses> {
     });
   }
   void _removeExpense(Expense expense){
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: const Text('Expense deleted.'),
+          duration: const Duration(seconds: 3),
+          action: SnackBarAction(
+            label: 'Undo',
+            onPressed: (){
+              setState(() {
+                _registeredExpenses.insert(expenseIndex, expense);
+              });
+            },
+          ),
+        ),
+    );
 
   }
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemove: _removeExpense,
+      );
+    }
     return  Scaffold(
       appBar: AppBar(
         actions: [
@@ -61,11 +86,9 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('Track your expenses here.'),
           Expanded(
-              child: ExpensesList(
-                expenses: _registeredExpenses,
-                onRemove: _removeExpense,
+              child: mainContent,
               ),
-          ),
+
 
         ],
       ),
